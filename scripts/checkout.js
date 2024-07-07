c6b00dd018921dc63476bf72cdadd5214ddd95e4
xiaodingding <rannuofli@gmail.com>
2024-07-07 12:09:58 +0200

    14n update on html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -36,7 +36,7 @@ cart.forEach((cartItem)=>{
                 </div>
                 <div class="product-quantity">
                   <span>
-                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                    Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
                   </span>
                   <span class="update-quantity-link link-primary js-update-quantity-link" data-product-id="${matchingProduct.id}">
                     Update
@@ -113,11 +113,20 @@ document.querySelectorAll('.js-save-quantity-link')
 .forEach((link)=>{
     link.addEventListener('click',()=>{
         const productId =link.dataset.productId;
+       
+        const quantityInput = document.querySelector(`.js-quantity-input-${productId}`);
+        const newQuantity = Number(quantityInput.value);
+        if(newQuantity<0 || newQuantity>1000){
+          alert('wrong number');
+          return;
+        }
+        updateQuantity(productId,newQuantity);
+
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
         container.classList.remove('is-editing-quantity');
-        const quantityInput = document.querySelector(`.js-quantity-input-${productId.id}`);
-        const newQuantity = Number(quantityInput.value);
-        updateQuantity(productId,newQuantity);
+        const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`);
+        quantityLabel.innerHTML = newQuantity;
+        updateCartQuantity();
     })
 })
 
