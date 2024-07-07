ae84a1c14522544fb7354fced68e6ea56a19216c
xiaodingding <rannuofli@gmail.com>
2024-07-07 10:48:28 +0200

    14d display cartquantity at homepage

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,7 @@
 import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 let productsHTML = '';
+updateCartQuantity();
 
 products.forEach((product) => {
   productsHTML += `
