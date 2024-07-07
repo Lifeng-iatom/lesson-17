074d7d55998db17129fd083527e6134b2c856ca1
xiaodingding <rannuofli@gmail.com>
2024-07-07 11:52:43 +0200

    14l

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateCartQuantity} from "../data/cart.js";
+import { cart, removeFromCart, calculateCartQuantity, updateQuantity} from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";   
 
@@ -41,7 +41,7 @@ cart.forEach((cartItem)=>{
                   <span class="update-quantity-link link-primary js-update-quantity-link" data-product-id="${matchingProduct.id}">
                     Update
                   </span>
-                  <input class="quantity-input">
+                  <input class="quantity-input js-quantity-input-${matchingProduct.id} ">
                   <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id="${matchingProduct.id}">Save</span>
                   <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
@@ -115,6 +115,9 @@ document.querySelectorAll('.js-save-quantity-link')
         const productId =link.dataset.productId;
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
         container.classList.remove('is-editing-quantity');
+        const quantityInput = document.querySelector(`.js-quantity-input-${productId.id}`);
+        const newQuantity = Number(quantityInput.value);
+        updateQuantity(productId,newQuantity);
     })
 })
 
