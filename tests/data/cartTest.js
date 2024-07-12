b5586fc9abe3ba6a82c5d95fa9ae1bd4da9559a5
xiaodingding <rannuofli@gmail.com>
2024-07-12 13:12:32 +0200

    16c and 16d

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,6 +1,9 @@
 import { cart, addToCart, loadFromStorage} from "../../data/cart.js";
 
 describe('Test suite:add to cart',()=>{
+      
+
+
       it('adds an existing product to the cart',()=>{
             
             spyOn(localStorage,'setItem');
@@ -16,8 +19,18 @@ describe('Test suite:add to cart',()=>{
             addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
             expect(cart.length).toEqual(1);
             expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+            expect(localStorage.setItem).toHaveBeenCalledWith('cart',JSON.stringify(
+                  [
+                        {
+                              productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                              quantity:2,
+                              deliveryOptionId:'1'    
+                        }
+                  ]
+            ));
             expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
             expect(cart[0].quantity).toEqual(2);
+           
 
 
 
@@ -37,6 +50,15 @@ describe('Test suite:add to cart',()=>{
             addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
             expect(cart.length).toEqual(1);
             expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+            expect(localStorage.setItem).toHaveBeenCalledWith('cart',JSON.stringify(
+                  [
+                        {
+                              productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                              quantity:1,
+                              deliveryOptionId:'1'    
+                        }
+                  ]
+            ));
             expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
             expect(cart[0].quantity).toEqual(1);
       });
--- tests/utils/moneyTest.js
+++ tests/utils/moneyTest.js
@@ -8,6 +8,10 @@ describe('test suite:formatCurrency',()=>{
       it('works with 0',()=>{
             expect(formatCurrency(0)).toEqual('0.00');
       })
+
+      it('works with negative number',()=>{
+            expect(formatCurrency(-100)).toEqual('-1.00');
+      })
       describe('rounding',()=>{
             it('round up',()=>{
                   expect(formatCurrency(2000.5)).toEqual('20.01');
