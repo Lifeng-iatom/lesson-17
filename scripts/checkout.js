11c91a60364ce65a06685d05e4a15d4117a5a8d7
xiaodingding <rannuofli@gmail.com>
2024-07-12 16:13:27 +0200

    creat oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { rendorPaymentSummary } from "./checkout/paymentSummary.js";
 import {renderCheckoutHeader} from "./checkout/checkoutHeader.js";
+import '../data/cart-oop.js';
 renderCheckoutHeader();
 renderOrderSummary();
 rendorPaymentSummary();
