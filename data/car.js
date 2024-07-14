edc87f1ae00218677d8c76067f5d366c7e46f53a
xiaodingding <rannuofli@gmail.com>
2024-07-14 11:37:22 +0200

    17d

--- data/car.js
+++ data/car.js
@@ -2,17 +2,22 @@ class Car{
       brand;
       model;
       speed=0;
+      isTrunkOpen = false;
+
       constructor(productDetails){
             this.brand = productDetails.brand;
             this.model = productDetails.model;
       }
 
       displayInfo(){
-            console.log(`${this.brand},${this.model}, speed: ${this.speed} km/h`);
+            const trunkStatus = this.isTrunkOpen ? 'open' : 'closed'
+            console.log(`${this.brand},${this.model}, speed: ${this.speed} km/h, trunk:${trunkStatus}`);
             
       }
       go(){
-            this.speed += 5 ;
+            if(!this.isTrunkOpen){
+                  this.speed += 5 ;
+            }
             if(this.speed >200){
                   this.speed = 200;
             }
@@ -22,6 +27,16 @@ class Car{
              if(this.speed < 0 ){
                   this.speed = 0;
              }
+      }
+
+      openTrunk(){
+            if(this.speed === 0){
+            this.isTrunkOpen = true;
+            }
+      }
+
+      closeTrunk(){
+            this.isTrunkOpen = false;
       }
 
 }
@@ -38,17 +53,21 @@ const car2 = new Car({
 
  console.log(car1);
  console.log(car2);
+
+ car1.openTrunk();
  car1.displayInfo();
- car2.displayInfo();
+ 
  car1.go();
+ car1.closeTrunk();
  car1.go();
  car1.go();
  car1.displayInfo();
  car1.brake();
  car1.displayInfo();
- car2.go();
- car2.go();
 
+ car2.displayInfo();
+ car2.go();
+ car2.go();
  car2.displayInfo();
  car2.brake();
  car2.displayInfo();
